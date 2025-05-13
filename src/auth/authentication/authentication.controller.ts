656b3261980b9d47b4bc1466449316f1365a72d4
viktor.k <viktor.kharts@gmail.com>
2025-05-13 19:34:26 +0200

    add documentation to authentication;

--- src/auth/authentication/authentication.controller.ts
+++ src/auth/authentication/authentication.controller.ts
@@ -13,17 +13,20 @@ import { Response } from 'express';
 import { AuthDecorator } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @AuthDecorator(AuthType.NONE)
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
+  @ApiResponse({ status: 200, description: 'Registers a new User.' })
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authenticationService.signUp(signUpDto);
   }
 
+  @ApiResponse({ status: 200, description: 'Sign an existing User in.' })
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
   async signIn(
@@ -40,6 +43,10 @@ export class AuthenticationController {
     return { refreshToken: token.refreshToken };
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Updates JWT tokens for an authenticated User.',
+  })
   @HttpCode(HttpStatus.OK)
   @Post('refresh-tokens')
   refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
